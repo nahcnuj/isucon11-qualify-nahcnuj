083d023b1b675a571fdebc2628f1052225a9dc8a
Junichi Hayashi <junchan.kcct@gmail.com>
2021-08-21 13:22:46 +0900

    fix

--- bin/get-nginx-log.sh
+++ bin/get-nginx-log.sh
@@ -4,4 +4,4 @@ for server in isucon11-qualify-{1,2,3}; do
     rsync -av ${server}:/var/log/nginx/access.log.ltsv /tmp/${server}.access.log.ltsv
     ssh $server sh -c "\"sudo truncate /var/log/nginx/access.log.ltsv --size 0\""
 done
-cat /tmp/*.access.log.ltsv >/tmp/access.log.ltsv
+cat /tmp/*.access.log.ltsv >var/access.log.ltsv
