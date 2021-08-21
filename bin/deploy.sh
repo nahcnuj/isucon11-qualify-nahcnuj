de190bdd08619859e404dd3b7a57d84400e83ef8
Junichi Hayashi <junchan.kcct@gmail.com>
2021-08-21 13:11:13 +0900

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -8,7 +8,7 @@ dryrun=$2
 RSYNC="rsync -rptD -c -v"
 
 ssh $server sh -c "\"cd webapp/perl \
-    && sudo $RSYNC etc/nginx /etc/nginx $dryrun \
-    && sudo $RSYNC etc/mysql /etc/mysql $dryrun \
+    && sudo $RSYNC etc/nginx /etc/ $dryrun \
+    && sudo $RSYNC etc/mysql /etc/ $dryrun \
     && sudo $RSYNC etc/systemd/system/*.service /etc/systemd/system/ $dryrun \
 \""
