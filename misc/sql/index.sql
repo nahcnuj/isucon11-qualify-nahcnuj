522e0184fd6028a4b728ec73f9924e802f5dfc01
Junichi Hayashi <junchan.kcct@gmail.com>
2021-08-21 16:45:51 +0900

    sql file for initialization

---
+++ misc/sql/index.sql
@@ -0,0 +1,6 @@
+CREATE INDEX `character` ON isu (`character`);
+CREATE INDEX `id__jia_user_id` ON isu (`id`,`jia_user_id`);
+CREATE INDEX `jia_isu_uuid__id` ON isu (`jia_isu_uuid`,`id` DESC);
+
+CREATE INDEX `jia_isu_uuid__timestamp` ON isu_condition (`jia_isu_uuid`, `timestamp` DESC);
+CREATE INDEX `jia_isu_uuid__id` ON isu_condition (`jia_isu_uuid`,`id` DESC);
